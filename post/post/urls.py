b405a58aece564d8c01a11186b490086c836f2db
ashok kumar <49492598+ashokpkumar@users.noreply.github.com>
2024-06-08 19:32:45 +0530

    Revert "cleanup script"
    This reverts commit 6855cee6af3c1dd2ac24ead6041ad62b49ca3fee.

--- post/post/urls.py
+++ post/post/urls.py
@@ -1,33 +1,41 @@
+"""
+URL configuration for post project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path
-
-from post_man.views import (
-    PostManView,
-    AuthView,
-    Logout,
-    Delivery,
-    Delivered,
-    NotReachable,
-    Damaged,
-    CsRejected,
-    AddOrder,
-    AddUser,
-    ResetOrder,
-    DeleteOrder,
-)
-
+from post_man.views import PostManView, AuthView,    Logout,Delivery,Delivered,NotReachable,Damaged,        CsRejected,AddOrder,AddUser,ResetOrder,DeleteOrder
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", PostManView.as_view(), name="postman"),
     path("authenticate", AuthView.as_view(), name="authenticate"),
     path("logout", Logout.as_view(), name="logout"),
     path("start", Delivery.as_view(), name="start"),
+
     path("delivered/<order_id>", Delivered.as_view(), name="delivered"),
     path("not_reachable/<order_id>", NotReachable.as_view(), name="not_reachable"),
     path("damaged/<order_id>", Damaged.as_view(), name="damaged"),
     path("cus_rejected/<order_id>", CsRejected.as_view(), name="cus_rejected"),
+
     path("add_user", AddUser.as_view(), name="AddUser"),
+    # path("add_order", AddOrder.as_view(), name="add_order"),
     path("reset_order", ResetOrder.as_view(), name="reset_order"),
     path("delete_order", DeleteOrder.as_view(), name="reset_order"),
     path("insert_order/<street>/<landmark>/<city>/<state>/<country>", AddOrder.as_view(), name="reset_order"),
+
+    
+
+
 ]
--- post/post_man/models.py
+++ post/post_man/models.py
@@ -1,15 +1,19 @@
 from django.db import models
+from django.db import models
+# from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+# Create your models here.
 class UserModel(models.Model):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
-    token = models.CharField(max_length=255, null=True, blank=True)
+    token = models.CharField(max_length=255,null=True, blank=True)
     token_created_date = models.DateTimeField(null=True, blank=True)
 
     def check_password(self, password):
         return password == self.password
-
+    
 class Order(models.Model):
     order_id = models.CharField(max_length=50, unique=True)
     customer_name = models.CharField(max_length=100)
@@ -29,11 +33,11 @@ class Order(models.Model):
     item_type = models.CharField(max_length=100)
     battery_included = models.BooleanField()
     expected_delivery_date = models.DateField()
-    priority = models.CharField(max_length=100, choices=[
-        ('two_hours', 'Two Hours'),
-        ('one_day', 'One Day'),
-        ('super_fast', 'Super Fast'),
-        ('normal', 'Normal')
+    priority = models.CharField(max_length=100,choices=[
+        ('two_hours', 'two_hours'),
+        ('one_day', 'one_day'),
+        ('super_fast', 'super_fast'),
+        ('normal', 'normal')
     ])
 
     order_status = models.CharField(max_length=100, choices=[
